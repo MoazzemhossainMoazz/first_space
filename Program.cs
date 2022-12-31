e08b0952d2188f3ffdbd5d4b4e2cccc1c52584d2
Moazz <moazzemh222@gmail.com>
2022-12-31 23:51:25 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using first_space;
+using second_space;
+
+namespace first_space
+{
+    class abc
+    {
+        public void func()
+        {
+            Console.WriteLine("Inside frist_space");
+        }
+    }
+}
+
+namespace second_space
+{
+    class efg
+    {
+        public void func()
+        {
+            Console.WriteLine("Inside second_space");
+        }
+    }
+}
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        abc fc = new abc();
+        efg sc = new efg();
+        fc.func();
+        sc.func();
+        Console.ReadKey();
+    }
+}
